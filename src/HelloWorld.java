e331cea1183a8f345fa5a9ce256c4677e15fafb2
Flavio Bossi <flavio.bossi@sinfonit.com>
2017-04-10 21:48:40 +0200

    V2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-system.out.println("HalloWelt3");
+System.out.println("HelloWorld");
 	}
 
 }
