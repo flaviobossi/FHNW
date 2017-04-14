38204f1111b791d51546a6b48a8c33762fb0053e
flaviobossi <flaviobossi@swissonline.ch>
2017-04-14 19:21:22 +0200

    Update HelloWorld.java

--- MyThirdGit/src/HelloWorld.java
+++ MyThirdGit/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("3th HelloWorld");
+System.out.println("3th HelloWorld bearbeitet auf gitHub");
 	}
 
 }
